045a3e644fb02d662648ed6e1105aa6475e0b174
Morteza Masumi <morteza.masumi@gmail.com>
2025-08-28 20:55:07 +0330

    fix: minor in fa translation

--- resources/lang/en/fb-auth.php
+++ resources/lang/en/fb-auth.php
@@ -1,38 +1,11 @@
 <?php
 
 return [
-    'gender' => [
-        'female' => 'Female',
-        'male' => 'Male',
-        'undefined' => 'Undefined',
-        'ms' => 'Ms.',
-        'mr' => 'Mr.',
-        'girl' => 'Girl',
-        'boy' => 'Boy',
-    ],
-    'marriage' => [
-        'single' => 'Single',
-        'married' => 'Married',
-        'unknown' => 'Unknown',
-    ],
     'form' => [
         'first_name' => 'First name',
         'last_name' => 'Last name',
-        'nid' => 'National ID number',
-        'nid_pass' => 'National ID/Passport number',
-        'gender' => 'Gender',
-        'birth_date' => 'Birth date',
         'mobile' => 'Mobile',
         'username' => 'Username',
-        'roles' => 'Roles',
-        'user_profile' => 'Profile',
-        'account' => 'Account',
-        'password' => 'Password',
-        'expiration_date' => 'Expiration date',
-        'force_change_password' => 'Force change password',
-        'profile' => [
-            'father_name' => 'Father name',
-        ],
     ],
     'notification' => [
         'title' => 'Profile updated successfully',
